87801ca3aa271d553610a7249322b9bd2005b9ed
0artorias <artyr27011997@mail.com>
2022-09-21 18:35:39 +0300

    Создание своего класса, текст и добавление стилей

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Text('Шапка'),
-    ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text(
+        'Dark side',
+        style: TextStyle(
+            fontSize: 20, color: Colors.red, fontFamily: 'Times New Roman'),
+      ),
+    );
+  }
+}
