59cc2357050da0086a1b25ed9fbf257255a223a7
0artorias <artyr27011997@mail.com>
2022-09-24 19:15:59 +0300

    Добавил переменную и состояния

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() => runApp(MaterialApp(
       home: UserPanel(),
     ));
 
-class UserPanel extends StatelessWidget {
+class UserPanel extends StatefulWidget {
   const UserPanel({super.key});
+
+  @override
+  State<UserPanel> createState() => _UserPanelState();
+}
+
+class _UserPanelState extends State<UserPanel> {
+  // Класс состояния
+  int _count = 0; // Указали переменную
 
   @override
   Widget build(BuildContext context) {
@@ -17,39 +25,55 @@ class UserPanel extends StatelessWidget {
         backgroundColor: Colors.black45,
       ),
       body: SafeArea(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              Padding(padding: EdgeInsets.only(top: 30)),
-              Text('Какой-то там текст',
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black87,
-                  )),
-              Padding(padding: EdgeInsets.only(top: 30)),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/image/Artyrito.jpg'),
-                radius: 80,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.mail_outline,
-                    size: 25,
-                  ),
-                  Padding(padding: EdgeInsets.only(left: 10)),
-                  Text(
-                    'Email@mail.ru',
-                    style: TextStyle(color: Colors.black87),
-                  )
-                ],
-              )
-            ],
-          ),
-        ],
-      )),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 30)),
+                Text('Какой-то там текст',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black87,
+                    )),
+                Padding(padding: EdgeInsets.only(top: 30)),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/image/Artyrito.jpg'),
+                  radius: 80,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.mail_outline,
+                      size: 25,
+                    ),
+                    Padding(padding: EdgeInsets.only(left: 10)),
+                    Text(
+                      'Email@mail.ru',
+                      style: TextStyle(color: Colors.black87),
+                    )
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(left: 10)),
+                Text(
+                  'Count: $_count', // тут показана эта переменная
+                  style: TextStyle(color: Colors.black87),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.ac_unit_rounded),
+        backgroundColor: Colors.amber,
+        onPressed: () {
+          setState(() {
+            // Этот метод нужен что бы пепеменную увеличивать
+            _count++;
+          });
+        },
+      ),
     );
   }
 }
