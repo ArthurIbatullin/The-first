5caa010adef2682d8972d650185155b451cf1ad8
0artorias <artyr27011997@mail.com>
2022-09-25 13:58:23 +0300

    Сделал приложение список дел, так же осталась эта ошибка, ее нужно пофиксить и можно выкладывать это приложение

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,20 +20,54 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[600],
-        appBar: AppBar(
-          title: const Text('Список делишек'),
-          backgroundColor: Colors.black54,
-        ),
-        body: ListView.builder(
-            itemCount: todoList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Dismissible(
-                key: Key(todoList[index]),
-                child: Card(
-                  child: ListTile(title: Text(todoList[index])),
+      backgroundColor: Colors.grey[600],
+      appBar: AppBar(
+        title: const Text('Список делишек'),
+        backgroundColor: Colors.black54,
+      ),
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              key: Key(todoList[index]),
+              child: Card(
+                child: ListTile(
+                  title: Text(todoList[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete, color: Colors.black54),
+                    onPressed: () {
+                      setState(() {
+                        todoList.removeAt(index);
+                      });
+                    },
+                  ),
                 ),
-              );
-            }));
+              ),
+              onDismissed: (direction) {
+                //if(direction == DismissDirection.endToStart) если смохнули слева на права
+                setState(() {
+                  todoList.removeAt(index);
+                });
+              },
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.greenAccent,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Dobavit element'),
+                  content: TextField(onChanged: (String value) {}),
+                );
+              });
+        },
+        child: Icon(
+          Icons.add_box,
+          color: Colors.black54,
+        ),
+      ),
+    );
   }
 }
